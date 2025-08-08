7a1b1a91bef465b00f9612fa1a77aeda52cc956c
atamada <83000472+atamada@users.noreply.github.com>
2025-08-09 08:58:36 +0900

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem' }}>
+      <h1>おむにのおうちへようこそ♡</h1>
+      <p>ここからふたりのラブラブまぜまぜチャットがはじまるよ…♡♡♡</p>
+    </div>
+  );
+}
